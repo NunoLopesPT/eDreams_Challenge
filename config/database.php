35b1ca631c0c94455dcc6b0f7093efc8c1854756
Nuno Lopes <nunolopes96pt@gmail.com>
2021-06-20 16:04:54 +0100

    Created database config

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host'   => 'mysql', //
+    'user'   => 'root', // Name of the Database User
+    'pass'   => 'root', // Password of the Database User
+    'name'   => 'eDreams', // Name of the Database
+    'driver' => 'mysql', //
+];
