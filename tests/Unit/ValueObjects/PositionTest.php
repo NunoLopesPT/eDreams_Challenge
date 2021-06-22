685a92b72245d9243fb4b011005515868b2892fe
Nuno Lopes <nunolopes96pt@gmail.com>
2021-06-22 20:30:50 +0100

    Created unit test for Position

---
+++ tests/Unit/ValueObjects/PositionTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace eDreams\Tests\Unit\ValueObjects;
+
+use eDreams\Domain\Exceptions\Game\ColumnWithNegativeNumberException;
+use eDreams\Domain\Exceptions\Game\RowWithNegativeNumberException;
+use eDreams\Domain\ValueObjects\TicTacToe\Position;
+use PHPUnit\Framework\TestCase;
+
+class PositionTest extends TestCase
+{
+    public function testGettersNewPosition(): void
+    {
+        $position = new Position(0, 1);
+
+        $this->assertEquals(0, $position->row());
+        $this->assertEquals(1, $position->column());
+    }
+
+    public function testCreatingPositionWithNegativeRow(): void
+    {
+        $this->expectException(RowWithNegativeNumberException::class);
+
+        new Position(-1, 0);
+    }
+
+    public function testCreatingPositionWithNegativeColumn(): void
+    {
+        $this->expectException(ColumnWithNegativeNumberException::class);
+
+        new Position(0, -1);
+    }
+
+    public function testPositionsAreEqual(): void
+    {
+        $position1 = new Position(0, 0);
+        $position2 = new Position(0, 0);
+
+        $this->assertTrue($position1->isEquals($position2));
+    }
+}
