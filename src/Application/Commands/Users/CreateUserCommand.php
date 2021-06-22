8a7f06153ee0767172142b07644555e2fc4ae3c4
Nuno Lopes <nunolopes96pt@gmail.com>
2021-06-22 20:48:47 +0100

    Create command to create user

---
+++ src/Application/Commands/Users/CreateUserCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace eDreams\Application\Commands\Users;
+
+use eDreams\Application\Commands\Command;
+use eDreams\Domain\Entities\User;
+use eDreams\Domain\Factories\Services\UserServiceFactory;
+
+class CreateUserCommand extends Command
+{
+    private static function validate(array $args): void
+    {
+        if (empty($args)) {
+            echo "Name of the User missing\n";
+            exit(1);
+        }
+    }
+
+    public static function handle(array $args = []): void
+    {
+        self::validate($args);
+
+        $service = UserServiceFactory::get();
+        $user = $service->create(new User($args[0]));
+
+        echo 'User created with id #' . $user->id() . "\n";
+        exit(0);
+    }
+}
