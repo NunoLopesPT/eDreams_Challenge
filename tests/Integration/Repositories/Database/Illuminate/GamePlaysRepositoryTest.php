2181dfc18d188c8c3aea2f2eb500be5b62a73411
Nuno Lopes <nunolopes96pt@gmail.com>
2021-06-22 18:18:57 +0100

    Create tests for game repositories

---
+++ tests/Integration/Repositories/Database/Illuminate/GamePlaysRepositoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace eDreams\Tests\Integration\Repositories\Database\Illuminate;
+
+use eDreams\Domain\Contracts\Repositories\GameRepository;
+use eDreams\Domain\Entities\Game;
+use eDreams\Domain\Entities\User;
+use eDreams\Domain\Factories\Repositories\GamePlaysRepositoryFactory;
+use eDreams\Domain\Factories\Repositories\GamesRepositoryFactory;
+use eDreams\Domain\Factories\Repositories\UsersRepositoryFactory;
+use eDreams\Domain\Repositories\Database\Illuminate\GamePlaysRepository;
+use eDreams\Domain\Repositories\Database\Illuminate\UsersRepository;
+use eDreams\Domain\ValueObjects\TicTacToe\Move;
+use eDreams\Domain\ValueObjects\TicTacToe\Position;
+use PHPUnit\Framework\TestCase;
+
+class GamePlaysRepositoryTest extends TestCase
+{
+    private GamePlaysRepository $gamePlaysRepository;
+    private GameRepository $gameRepository;
+    private UsersRepository $usersRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->gamePlaysRepository = GamePlaysRepositoryFactory::get();
+        $this->gameRepository = GamesRepositoryFactory::get();
+        $this->usersRepository = UsersRepositoryFactory::get();
+    }
+
+    private function createGame(): Game
+    {
+        $user1 = $this->usersRepository->create(new User('user1'));
+        $user2 = $this->usersRepository->create(new User('user2'));
+
+        $game = new Game(
+            $user1,
+            $user2,
+            null,
+            false,
+            []
+        );
+
+        return $this->gameRepository->create($game);
+    }
+
+    public function testCreateAndGetAllMovesOfGame(): void
+    {
+        $game = $this->createGame();
+
+        $position = new Position(0, 0);
+        $move = new Move($position, $game->user1());
+        $game->makeMove($move);
+
+        $this->gamePlaysRepository->createNewMoves($game);
+
+        $moves = $this->gamePlaysRepository->getAllByGameId($game->id());
+
+        $this->assertEquals(1, count($moves));
+        $this->assertTrue($position->isEquals($moves[0]->position()));
+    }
+}
---
+++ tests/Integration/Repositories/Database/Illuminate/GameRepositoryTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace eDreams\Tests\Integration\Repositories\Database\Illuminate;
+
+use eDreams\Domain\Entities\Game;
+use eDreams\Domain\Entities\User;
+use eDreams\Domain\Exceptions\Game\GameAlreadyCreatedException;
+use eDreams\Domain\Exceptions\Game\GameNotFoundException;
+use eDreams\Domain\Factories\Repositories\GamesRepositoryFactory;
+use eDreams\Domain\Factories\Repositories\UsersRepositoryFactory;
+use eDreams\Domain\Repositories\Database\Illuminate\GamesRepository;
+use eDreams\Domain\Repositories\Database\Illuminate\UsersRepository;
+use PHPUnit\Framework\TestCase;
+
+class GameRepositoryTest extends TestCase
+{
+    private GamesRepository $gamesRepository;
+    private UsersRepository $usersRepository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->gamesRepository = GamesRepositoryFactory::get();
+        $this->usersRepository = UsersRepositoryFactory::get();
+    }
+
+    public function testCreateGame(): void
+    {
+        $user1 = $this->usersRepository->create(new User('user1'));
+        $user2 = $this->usersRepository->create(new User('user2'));
+
+        $game = new Game(
+            $user1,
+            $user2,
+            null,
+            false,
+            []);
+
+        $game = $this->gamesRepository->create($game);
+
+        $this->assertNotNull($game->id());
+        $this->assertEquals($user1->id(), $game->user1()->id());
+        $this->assertEquals($user2->id(), $game->user2()->id());
+        $this->assertNull($game->winner());
+        $this->assertNull($game->winnerId());
+        $this->assertFalse($game->isFinished());
+    }
+
+    public function testTryCreateGameAlreadyCreated(): void
+    {
+        $user1 = $this->usersRepository->create(new User('user1'));
+        $user2 = $this->usersRepository->create(new User('user2'));
+
+        $game = new Game(
+            $user1,
+            $user2,
+            null,
+            false,
+            [],
+            1);
+
+        $this->expectException(GameAlreadyCreatedException::class);
+
+        $this->gamesRepository->create($game);
+    }
+
+    public function testGetCreatedGameById(): void
+    {
+        $user1 = $this->usersRepository->create(new User('user1'));
+        $user2 = $this->usersRepository->create(new User('user2'));
+
+        $game = new Game(
+            $user1,
+            $user2,
+            null,
+            false,
+            []);
+
+        $game = $this->gamesRepository->create($game);
+
+        $this->assertNotNull($this->gamesRepository->getById($game->id()));
+    }
+
+    public function testGetGameThatDoesntExists(): void
+    {
+        $this->expectException(GameNotFoundException::class);
+
+        $this->gamesRepository->getById(2147483647);
+    }
+
+    public function testCreateMovesGame(): void
+    {
+        $user1 = $this->usersRepository->create(new User('user1'));
+        $user2 = $this->usersRepository->create(new User('user2'));
+
+        $game = new Game(
+            $user1,
+            $user2,
+            null,
+            false,
+            []);
+
+        $game = $this->gamesRepository->create($game);
+
+        $game->setFinished($user2);
+
+        $this->gamesRepository->update($game);
+
+        $game = $this->gamesRepository->getById($game->id());
+
+        $this->assertTrue($game->isFinished());
+        $this->assertEquals($game->winnerId(), $user2->id());
+    }
+}
