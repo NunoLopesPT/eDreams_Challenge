7828e50a3737efaa69d05010c951c0db9816bc0b
Nuno Lopes <nunolopes96pt@gmail.com>
2021-06-22 22:01:51 +0100

    Update name of database user

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'host'   => 'mysql', //
-    'user'   => 'root', // Name of the Database User
+    'user'   => 'eDreams', // Name of the Database User
     'pass'   => 'root', // Password of the Database User
     'name'   => 'eDreams', // Name of the Database
     'driver' => 'mysql', //
